d46e83155ecbd47c68ac9e7a6957a89504755ff1
dipakbaldaniya <98524455+dipakbaldaniya@users.noreply.github.com>
2023-09-26 13:35:15 +0530

    spalsh screen add

--- src/splash.js
+++ src/splash.js
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 
-const home = () => {
+const splash = () => {
   return (
     <View>
-      <Text>home</Text>
+      <Text>splash</Text>
     </View>
   );
 };
 
-export default home;
+export default splash;
