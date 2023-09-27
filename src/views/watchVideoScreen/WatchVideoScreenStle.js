da6a6a91db0ceef058ae18c6f1e07c84d2bc8a23
mayankpambhar <101552233+mayankpambhar@users.noreply.github.com>
2023-09-27 18:02:24 +0530

    watch and earn screen

---
+++ src/views/watchVideoScreen/WatchVideoScreenStle.js
@@ -0,0 +1,51 @@
+import {StyleSheet} from 'react-native';
+import {useMemo} from 'react';
+import {useResponsiveScreen} from '../../Hooks/useResponsiveScreen';
+
+export const useWatchAndEarnStyle = () => {
+  const {hp, wp} = useResponsiveScreen();
+
+  const style = useMemo(() => {
+    return StyleSheet.create({
+      mainView: {
+        height: '100%',
+        width: '100%',
+      },
+      buttonView: {
+        height: '100%',
+        marginTop: hp(50),
+      },
+      coinTextView: {
+        fontSize: 25,
+        fontFamily: 'BalooBhai2-SemiBold',
+        color: '#000',
+        alignSelf: 'flex-end',
+        marginEnd: wp(10),
+        marginTop: hp(10),
+      },
+      titleView: {
+        fontFamily: 'BalooBhai2-SemiBold',
+        fontSize: 30,
+        color: '#000',
+        marginTop: hp(40),
+        alignSelf: 'center',
+      },
+      submit: {
+        //         marginTop: hp(130),
+        alignSelf: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: hp(50),
+        width: wp(200),
+        borderRadius: hp(10),
+        backgroundColor: '#32CCC3',
+      },
+      submitText: {
+        fontSize: 19,
+        fontFamily: 'BalooBhai2-SemiBold',
+        color: '#000',
+      },
+    });
+  }, [hp, wp]);
+  return style;
+};
--- src/views/watchVideoScreen/watchVideoScreen.js
+++ src/views/watchVideoScreen/watchVideoScreen.js
@@ -1,10 +1,19 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
+import {useWatchAndEarnStyle} from './WatchVideoScreenStle';
 
 const WatchVideoScreen = () => {
+  const styles = useWatchAndEarnStyle();
+
   return (
-    <View>
-      <Text>WatchVideoScreen</Text>
+    <View style={styles.mainView}>
+      <Text style={styles.coinTextView}>4000</Text>
+      <Text style={styles.titleView}>Watch Ads To Get Coins</Text>
+      <View style={styles.buttonView}>
+        <TouchableOpacity style={styles.submit} onPress={() => {}}>
+          <Text style={styles.submitText}>Watch Ads</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
